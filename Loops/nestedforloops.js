e84d39129203241cae245aab2571879186ad1377
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-07-16 10:30:48 +0530

    completed the day 4 challenge

---
+++ Loops/nestedforloops.js
@@ -0,0 +1,29 @@
+// Task 7
+/*for(let i = 0; i < 5; i++){
+    let row = '';
+    for(let j = 0; j <= i; j++){
+        row += '*';
+        row += " ";
+    }
+    console.log(row);
+}*/
+
+//Task 8
+/*let i = 1;
+while(i <= 10){
+    if(i == 5){
+        i++;
+        continue;
+    }
+    console.log(i++);
+}*/
+
+
+// Task 8
+let i = 1;
+while(i <= 10){
+    if(i == 7){
+        break;
+    }
+    console.log(i++);
+}
---
+++ Loops/printno.js
@@ -0,0 +1,62 @@
+// Task 1
+/*for(let i = 1; i <= 10; i++){
+    console.log(i);
+}*/
+
+/*let i = 1;
+do{
+    console.log(i++);
+}while(i <= 10)
+
+while(i <= 10){
+    console.log(i++);
+}*/
+
+//Task 2
+/*let i = 1;
+while(i <= 10){
+    console.log(5 * i);
+    i++;
+}*/
+
+//Task 3
+/*let i = 1;
+let sum = 0;
+while(i <= 10){
+    sum += i;
+    i++;
+}
+console.log(sum);
+*/
+
+//Task 4
+/*let i = 10;
+while(i > 0){
+    console.log(i--);
+}*/
+
+//Task 5
+/*let i = 1;
+do {
+  console.log(i++);
+} while (i <= 5);
+*/
+
+
+//Task 6
+/*
+let i = 5;
+let fact = 1;
+
+if (i == 0) {
+    console.log(1);
+    return;
+}
+do {
+  fact = fact * i;
+  i--;
+} while (i > 0);
+console.log(fact);
+*/
+
+
