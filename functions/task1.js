6466846da1e2a8ea551b2f37e1b6f44dffb150c5
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-07-17 13:35:03 +0530

    Day 5 functions completed

---
+++ functions/task1.js
@@ -0,0 +1,57 @@
+//Task 1
+/*function checkEvenOdd(num) {
+  if (num % 2 == 0) {
+    return "Even number";
+  } else {
+    return "Odd number";
+  }
+}
+
+const val = checkEvenOdd(1890);
+console.log(val);
+
+//Task 2
+function squareNumber(num) {
+    return num * num;
+}
+
+console.log(squareNumber(10))
+
+// Task 3
+function getMax(a, b){
+    if(a > b){
+        return a;
+    }
+    else if(a == b){
+        return "Both are equal";
+    }
+    else{
+        return b;
+}
+}
+console.log(getMax(5, 5));
+
+
+//Task 4
+function strCon(num1, num2){
+    return num1 +" " + num2;
+}
+console.log(strCon("Harry", "Pandey"));
+
+
+//Task 5
+const sumofTwoNumber = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(sumofTwoNumber(3, 99878979));
+
+
+//Task 6
+const checkChar = (str) =>{
+    const val = str.includes("t");
+    return val;
+}
+
+console.log(checkChar("Utkars"))
+*/
---
+++ functions/task2.js
@@ -0,0 +1,30 @@
+//Task 7
+/*
+function Twoval(num1, num2 = 5){
+    return num1 * num2;
+
+console.log(Twoval(4));
+
+//Task 8
+function greet(name, age = 67){
+    return `Hii, ${name} Your age is ${age}`
+}
+
+console.log(greet("John", 34))
+*/
+
+//Task 9
+/*function callmeAgain(){
+    console.log("Hii");
+}
+function higherOrder(callback, num){
+    if(num == 0){
+        return;
+    }
+
+    callback();
+    higherOrder(callback, num - 1);
+}
+
+higherOrder(callmeAgain, 5);*/
+
