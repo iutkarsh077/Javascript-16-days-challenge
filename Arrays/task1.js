f6f9b6b0ce8bb85b66a583dab0f84d3a9d70ce36
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-07-18 10:48:27 +0530

    Day 6 : Arrays done

---
+++ Arrays/task1.js
@@ -0,0 +1,28 @@
+//Task 1
+/*
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+
+//Task 2
+let arr1 = [5, 1, 2, 7, 8, 9, 3, 2, 4];
+console.log(arr1[0], arr1[arr1.length - 1])
+
+// Task 3
+let arr2 = [4, 1, 2, 3];
+arr2.push(90);
+console.log(arr2)
+
+//Task 4
+let arr3 = [5, 1, 2, 6, 3, 4, 2, 90];
+console.log(arr3);
+arr3.pop();
+console.log(arr3);
+
+// Task 5
+let arr4 = [7, 1, 2, 3, 7, 6, 8];
+console.log(arr4);
+arr4.shift();
+arr4.unshift(8232);
+console.log(arr4);
+*/
+
---
+++ Arrays/task2.js
@@ -0,0 +1,46 @@
+//Task 7
+/*
+let arr1 = [1, 2, 3, 4, 5, 6, 7];
+let arr2 = arr1.map((val)=>(
+     val * val
+))
+console.log("Arr1: ", arr1);
+console.log("Arr2: ", arr2);
+
+//Task 8
+let arr3 = [4, 1, 3, 7, 6, 8, 9, 6, 4, 1, 90, 79];
+let filter = arr3.filter((val)=>{
+    return val % 2 == 0
+})
+console.log("The even numbers for filter is: ", filter);
+
+//Task 9
+let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let red = arr4.reduce((total, val)=>{
+    console.log(total)
+    return total + val
+})
+console.log(red)
+
+
+//Task 10
+let arr5 = [3, 1, 6, 4, 3, 7, 8, 9];
+for(let i = 0; i < arr5.length; i++){
+    console.log(arr5[i]);
+}
+
+
+//Task 11
+let arr6 = [4, 1, 2, 7, 6, 8, 9 ,3];
+arr6.forEach((val, idx)=>(
+    console.log("The value at index ",idx, "is ",  val)
+))
+
+
+//Task 12 && task 13
+let twoArr = [[3, 1, 2], [5, 1, 2], [7, 5, 4] ,[4, 1, 8]];
+console.log(twoArr);
+console.log(twoArr[3][2]);
+*/
+
+
